3c44b633bcf937a6934632433d5c55cec813f716
Lunia <lunia0251@proton.me>
2026-09-19 10:36:37 +0700

    SettingsGoogle: Initial AOSP override
    Change-Id: Ia5ef59f2f9990b456f013acb5649107f7d9b0ad3

---
+++ src/com/google/android/settings/generalaccess/overlay/SettingsGoogleGeneralAccessFeatureFactoryImpl.kt
@@ -0,0 +1,5 @@
+package com.google.android.settings.generalaccess.overlay
+
+import com.google.android.settings.overlay.FeatureFactoryImpl
+
+open class SettingsGoogleGeneralAccessFeatureFactoryImpl : FeatureFactoryImpl()
